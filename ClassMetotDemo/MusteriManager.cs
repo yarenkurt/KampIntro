684b5d834b677a8776191faf34714cf3427932c6
Yaren <yarenkurt078@outlook.com>
2021-12-02 21:24:01 +0300

    'Odev'

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi+musteri.Soyadi+" isimli müşteri sisteme eklenmiştir.");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.HesapNumarasi + " numaralı hesaba ait bilgiler: \n"+
+                "Müşteri Adı: "+ musteri.Adi + "\n" +
+                "Müşteri Soyadı: "+ musteri.Soyadi + "\n" +
+                "Müşteri TC'si: " + musteri.Tc + "\n");
+            }
+            
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + musteri.Soyadi + " isimli müşteri sistemden silinmiştir.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.Adi = "Yaren";
+            m1.Soyadi = "Kurt";
+            m1.Tc = 11111111111;
+            m1.HesapNumarasi = 1234567890;
+
+            Musteri m2 = new Musteri();
+            m2.Adi = "Abdi";
+            m2.Soyadi = "Kurt";
+            m2.Tc = 22222222222;
+            m2.HesapNumarasi = 123453890;
+
+            Musteri m3 = new Musteri();
+            m3.Adi = "Engin";
+            m3.Soyadi = "Demiroğ";
+            m3.Tc = 33333333333;
+            m3.HesapNumarasi = 1239167890;
+
+
+            Console.WriteLine("-------------EKLEME-----------");
+            MusteriManager manager = new MusteriManager();
+            manager.Ekle(m1);
+            manager.Ekle(m2);
+            manager.Ekle(m3);
+            Console.WriteLine("\n -------------LİSTELEME-----------");
+            Musteri[] musteriler = new Musteri[] {m1, m2, m3};
+            manager.Listele(musteriler);
+            Console.WriteLine("-------------SİLME-----------");
+            manager.Silme(m1);
+        }
+    }
+}
